c97be0b8b74dd4b424c6b42fe4910a35bfb670ef
Magnus <dorfmagnus@gmail.com>
2022-11-20 11:25:56 +0300

    New Arguments

---
+++ L1_Python_Interaction.py
@@ -0,0 +1,11 @@
+print('Hello World!')   #Hello World!
+print("School!")    #School !
+print("I'm happy to learn!")    # I'm happy to learn!
+
+print('3.14')  # class <str>
+print(3.14)  # class <float>
+print(3)  # class <int>
+# ex : print(type('3.14'))
+
+
+
